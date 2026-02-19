fc1da9038f4a2a3711970ddab915ed73f5f03e86
Terminal: Select Default Profile <Terminal: Select Default Profile>
2026-02-19 23:17:16 +0530

    Day-4

--- src/app.js
+++ src/app.js
@@ -1,5 +1,46 @@
-const express = reqiure('express')
+const express = require('express')
 
 const app = express()
+
+app.use(express.json())
+
+const notes = [
+    {
+        title:"test title1",
+        description: "description 1"
+    }
+]
+
+app.get("/", (req, res)=>{
+    res.send("hello world")
+})
+
+app.post("/notes", (req, res)=>{
+    console.log(req.body)
+    notes.push(req.body)
+    console.log(notes);
+    
+
+    res.send("note created")
+})
+
+app.get("/notes", (req, res)=>{
+    res.send(notes)
+})
+
+app.delete("/notes/:index", (req, res)=>{
+   delete notes[req.params.index];
+
+   res.send("note deleted successfully")
+    
+})
+
+app.patch("/notes/:index", (req, res)=>{
+
+    notes[ req.params.index ].description = req.body.description
+
+    res.send('Note updated successfully')
+
+})
 
 module.exports = app 
